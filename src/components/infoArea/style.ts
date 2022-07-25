7cb7dea3fc6d2501d28f28a2d78006fb11eaacb4
Everton Fernandes <evertonfernandes.dev@gmail.com>
2022-07-25 17:03:18 -0300

    stylesInfoArea

---
+++ src/components/infoArea/style.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #fff;
+    box-shadow: 0px 0px 5px #ccc;
+    border-radius: 10px;
+    display: flex;
+    align-items: center;
+    margin-top: -40px;
+    padding: 20px;
+`
+export const MonthArea = styled.div`
+   flex: 1;
+   display: flex;
+   align-items: center;
+`
+export const monthArrow = styled.div`
+    width: 40px;
+    text-align: center;
+    font-size: 25px;
+    cursor: pointer;
+`
+export const MonthTitle = styled.div`
+    flex: 1;
+    text-align: center;
+`
+export const ResumeArea = styled.div`
+    flex: 2;
+    display: flex;
+` 
