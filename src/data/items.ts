edc9af181f5cd839ab2f64ee10b0f544fff2b435
Everton Fernandes <evertonfernandes.dev@gmail.com>
2022-07-25 17:05:14 -0300

    items

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item'
+
+export const items: Item[] = [
+    { date: new Date(2022, 5, 27), category: 'food', title: 'McDonalds', value: 32.12 },
+    { date: new Date(2022, 5, 27), category: 'food', title: 'Burguer King', value: 17.12 },
+    { date: new Date(2022, 5, 27), category: 'rent', title: 'Aluguel apt', value: 2300 },
+    { date: new Date(2022, 6, 27), category: 'salary', title: 'Salário ACME', value: 4500 },
+]
