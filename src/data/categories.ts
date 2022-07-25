35a3c0f4476fc9bd9bcd485b1dadc5c760f20f8d
Everton Fernandes <evertonfernandes.dev@gmail.com>
2022-07-25 17:05:07 -0300

    categories

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import { Category } from './../types/Category';
+
+export const categories: Category = {
+    food: { title: 'alimentação', color: 'blue', expense: true },
+    rent: { title: 'Aluguel', color: 'brown', expense: true },
+    salary: { title: 'Salário', color: 'green', expense: false }
+}
